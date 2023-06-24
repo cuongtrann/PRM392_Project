0240cf460f3a19eb007338665f7685ed84a2f39f
Cường Trần <cuongtn251002@gmail.com>
2023-06-24 21:41:29 +0700

    Initial commit.

---
+++ app/src/main/java/com/example/project_prm392/ChatWithShopActivity.java
@@ -0,0 +1,14 @@
+package com.example.project_prm392;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatWithShopActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_with_shop);
+    }
+}
---
+++ app/src/main/java/com/example/project_prm392/ProductDetailScreen/ProductDetailActivity.java
@@ -0,0 +1,63 @@
+package com.example.project_prm392.ProductDetailScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.project_prm392.R;
+import com.google.android.material.tabs.TabLayout;
+
+public class ProductDetailActivity extends AppCompatActivity {
+
+    private ProductPagerAdapter productPagerAdapter;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+
+        // Khởi tạo ViewPager và TabLayout
+        viewPager = findViewById(R.id.view_pager);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+
+        // Khởi tạo Adapter cho ViewPager
+        productPagerAdapter = new ProductPagerAdapter(getSupportFragmentManager());
+        productPagerAdapter.addFragment(new DescriptionFragment(), "Description");
+        productPagerAdapter.addFragment(new ReviewFragment(), "Review");
+        productPagerAdapter.addFragment(new AdditionalInfoFragment(), "Additional Information");
+
+        // Thiết lập Adapter cho ViewPager và kết nối ViewPager với TabLayout
+        viewPager.setAdapter(productPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                // Không cần thực hiện
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Fragment fragment = productPagerAdapter.getItem(position);
+                View view = fragment.getView();
+                if (view != null) {
+                    view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+                    int height = view.getMeasuredHeight(); // Tính toán chiều cao của Fragment hiện tại
+                    viewPager.getLayoutParams().height = height; // Đặt chiều cao của ViewPager bằng giá trị tính toán
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                // Không cần thực hiện
+            }
+        });
+
+    }
+
+
+
+}
