62d4f88ac3eb2a1f2c767413f476d2697eb06602
qa0108 <100855723+qa0108@users.noreply.github.com>
2023-07-04 22:07:10 +0700

    add btn listener

--- app/src/main/java/com/example/project_prm392/activities/LoginActivity.java
+++ app/src/main/java/com/example/project_prm392/activities/LoginActivity.java
@@ -2,7 +2,11 @@ package com.example.project_prm392.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.project_prm392.R;
 
@@ -12,5 +16,33 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        SetBtnLoginOnClickEvent();
+        SetBtnSignUpOnClickEvent();
+    }
+
+    private void SetBtnLoginOnClickEvent(){
+        Button btnLogin = (Button) findViewById(R.id.buttonLogin);
+        btnLogin.setOnClickListener(v -> OnClickBtnLogin());
+    }
+
+    private void OnClickBtnLogin(){
+        String email = ((EditText) findViewById(R.id.et_email)).getText().toString();
+        String password = ((EditText) findViewById(R.id.et_password)).getText().toString();
+
+    }
+
+    private void SetBtnSignUpOnClickEvent(){
+        Button btnSignUp = (Button) findViewById(R.id.buttonSignup);
+        btnSignUp.setOnClickListener(v -> OnClickBtnSignUp());
+    }
+
+    private void OnClickBtnSignUp(){
+        ChangeActivity(SignUpActivity.class);
+    }
+
+    private void ChangeActivity(Class target){
+        Intent intent = new Intent(this, target);
+        startActivity(intent);
     }
 }
