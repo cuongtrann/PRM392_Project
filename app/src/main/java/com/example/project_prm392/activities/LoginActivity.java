64d1ea3cce81ecc40addb63a5760b59234f77caa
anhndq <quanganh01082002@gmail.com>
2023-07-07 17:52:40 +0700

    add logic login and sign up

--- app/src/main/java/com/example/project_prm392/activities/LoginActivity.java
+++ app/src/main/java/com/example/project_prm392/activities/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.project_prm392.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,15 +8,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.project_prm392.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText email, password;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        email = findViewById(R.id.et_loginEmail);
+        password = findViewById(R.id.et_loginPassword);
 
         SetBtnLoginOnClickEvent();
         SetBtnSignUpOnClickEvent();
@@ -27,16 +41,21 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void OnClickBtnLogin(){
-        String email = ((EditText) findViewById(R.id.et_email)).getText().toString();
-        String password = ((EditText) findViewById(R.id.et_password)).getText().toString();
+        String inputEmail = email.getText().toString();
+        String inputPassword = password.getText().toString();
 
-        //authenticate
-        if(true){
+        firebaseAuth.signInWithEmailAndPassword(inputEmail,inputPassword).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    //change to main activity
+                }
 
-        }
-        else{
-
-        }
+                if(task.isCanceled()){
+                    Toast.makeText(LoginActivity.this, "Error: " + task.getException(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     private void SetBtnSignUpOnClickEvent(){
--- app/src/main/java/com/example/project_prm392/activities/SignUpActivity.java
+++ app/src/main/java/com/example/project_prm392/activities/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.example.project_prm392.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,19 +11,23 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.project_prm392.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    EditText name;
-    EditText email;
-    EditText password;
-    EditText cfPassword;
+    EditText name, email, password, cfPassword;
     Button btnSignUp, btnLogin;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         name = findViewById(R.id.et_name);
         email = findViewById(R.id.et_email);
@@ -39,34 +44,49 @@ public class SignUpActivity extends AppCompatActivity {
         });
     }
 
-    private void OnClickBtnSignUp(){
+    private void OnClickBtnSignUp() {
         String inputName = name.getText().toString();
         String inputEmail = email.getText().toString();
         String inputPassword = password.getText().toString();
         String inputCfPassword = cfPassword.getText().toString();
 
-        if(TextUtils.isEmpty(inputName)){
-            Toast.makeText(this,"Enter name!", Toast.LENGTH_SHORT).show();
+        if (TextUtils.isEmpty(inputName)) {
+            Toast.makeText(this, "Enter name!", Toast.LENGTH_SHORT).show();
+            return;
         }
 
-        if(TextUtils.isEmpty(inputEmail)){
-            Toast.makeText(this,"Enter email!", Toast.LENGTH_SHORT).show();
+        if (TextUtils.isEmpty(inputEmail)) {
+            Toast.makeText(this, "Enter email!", Toast.LENGTH_SHORT).show();
+            return;
         }
 
-        if(TextUtils.isEmpty(inputPassword)){
-            Toast.makeText(this,"Enter password!", Toast.LENGTH_SHORT).show();
+        if (TextUtils.isEmpty(inputPassword)) {
+            Toast.makeText(this, "Enter password!", Toast.LENGTH_SHORT).show();
+            return;
         }
 
-        if(TextUtils.isEmpty(inputCfPassword)){
-            Toast.makeText(this,"Confirm your password!", Toast.LENGTH_SHORT).show();
+        if (TextUtils.isEmpty(inputCfPassword)) {
+            Toast.makeText(this, "Confirm your password!", Toast.LENGTH_SHORT).show();
+            return;
         }
 
-        if(!inputPassword.equals(inputCfPassword)){
-            Toast.makeText(this,"Password did not matched!", Toast.LENGTH_SHORT).show();
+        if (!inputPassword.equals(inputCfPassword)) {
+            Toast.makeText(this, "Password did not matched!", Toast.LENGTH_SHORT).show();
+            return;
         }
+
+        //TODO: check whether email has existed
+
+
+        firebaseAuth.createUserWithEmailAndPassword(inputEmail, inputPassword).addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                OnClickBtnLogin();
+            }
+        });
     }
 
-    private void OnClickBtnLogin(){
+    private void OnClickBtnLogin() {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
